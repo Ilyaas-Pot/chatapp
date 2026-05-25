95437835b9f8b6c78ddf06acdcfd381dde341ddd
Ilyaas-Pot <ST10508067@rcconnect.edu.za>
2026-05-25 20:46:48 +0200

    Test: creating tests

--- src/test/java/TestMessage/MessageTest.java
+++ src/test/java/TestMessage/MessageTest.java
@@ -14,45 +14,6 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class MessageTest {
     
-   @Test
-    public void testMessageLengthSuccess() {
-
-        Message msg = new Message(1, "+27838968976", "Hello there");
-
-        assertTrue(msg.checkMessageID());
-    }
-
-    @Test
-    public void testRecipientCellSuccess() {
-
-        Message msg = new Message(1, "+27838968976", "Hello there");
-
-        assertTrue(msg.checkRecipientCell());
-    }
-
-    @Test
-    public void testRecipientCellFail() {
-
-        Message msg = new Message(1, "0838968976", "Hello there");
-
-        assertFalse(msg.checkRecipientCell());
-    }
-
-    @Test
-    public void testMessageHash() {
-
-        Message msg = new Message(1, "+27838968976", "Hi Mike");
-
-        assertNotNull(msg.createMessageHash());
-    }
-
-    @Test
-    public void testTotalMessages() {
-
-        new Message(1, "+27838968976", "Hello");
-        new Message(2, "+27838968976", "How are you");
-
-        assertEquals(2, Message.returnTotalMessages());
-    }
+   
     
 }
